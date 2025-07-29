7feec83135e0a6cc966ebba03da9a1b4e85a25fa
PLMatlhape <ogonatau@gmail.com>
2025-07-29 09:23:31 +0200

    removed unused

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './navBar.css' 
 
 export const NavBar = () => {
--- src/components/PromoBanner.tsx
+++ src/components/PromoBanner.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./promoBanner.css";
 
 export const PromoBanner = () => {
